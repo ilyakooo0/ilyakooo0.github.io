aeee383690b14d10a96475044191ebfd61322af7
ilyakooo0 <ilyakooo0@gmail.com>
2018-12-22 16:13:26 +0300

    Update diskra/sosalovo

--- javascript/diskra.sosalovo.js
+++ javascript/diskra.sosalovo.js
@@ -1,3 +1,7 @@
+function round(value, decimals = 3) {
+  return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
 function update() {
 	write()
 	
@@ -7,20 +11,20 @@ function update() {
 	result.text("")
 	text.text("")
 	
-	let kr3 = parseInt($("#KR3").val())
-	let dz3 = parseInt($("#DZ3").val())
-	let kr4 = parseInt($("#KR4").val())
-	let dz4 = parseInt($("#DZ4").val())
-	let mit = parseInt($("#MIT").val())
+	let kr3 = round(parseFloat($("#KR3").val()))
+	let dz3 = round(parseFloat($("#DZ3").val()))
+	let kr4 = round(parseFloat($("#KR4").val()))
+	let dz4 = round(parseFloat($("#DZ4").val()))
+	let mit = round(parseFloat($("#MIT").val()))
 	
 	if (isNaN(kr3) || isNaN(dz3) || isNaN(kr4) || isNaN(dz4) || isNaN(mit)) {
 		return
 	}
 	
-	let d = Math.round(0.62*Math.min(dz3, dz4) + 0.38*Math.max(dz3, dz4))
+	let d = round((0.62*Math.min(dz3, dz4) + 0.38*Math.max(dz3, dz4)))
 	
-	let k = Math.round(0.62*Math.min(kr3, kr4) + 0.38*Math.max(kr3, kr4))
-	let o = 0.62*Math.min(d, k) + 0.38*Math.max(d, k)
+	let k = round(0.62*Math.min(kr3, kr4) + 0.38*Math.max(kr3, kr4))
+	let o = Math.round(0.62*Math.min(d, k) + 0.38*Math.max(d, k))
 	let r = Math.round(0.8*Math.round(o) + 0.2*mit)
 	
 	result.text(r)
